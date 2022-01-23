b29450d92552b6ab30af4870578433c59ffc155a
Ferandamejia <94657412+Ferandamejia@users.noreply.github.com>
2022-01-23 15:15:28 -0600

    Add files via upload

---
+++ PROC9_P5Juego-main/sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+
+ if (keyIsDown(RIGHT_ARROW)){
+background ("DarkSeaGreen")
+ }
+if (keyIsDown(LEFT_ARROW)){
+   background("RosyBrown")
+ }
+
+ if (keyIsDown(UP_ARROW)){
+   background("DarkSlateGray")
+}
+
+if (keyIsDown(DOWN_ARROW)){
+   background("DarkSalmon")
+}
+}
+
+
+
+
